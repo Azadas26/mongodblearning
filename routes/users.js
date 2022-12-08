3328a5385fdea61a64045e7a8914ad1f256dd955
Azadas26 <azad45518@gmail.com>
2022-12-08 16:45:07 +0530

    End

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users');
 });
+router.post('/login', function(req, res, next) {
+ 
+  
+  res.send("Sucess!!!!!!!!!!!!")
+
+  MongoClient.connect("mongodb://localhost:27017",(err,clint)=>{
+    if(err)
+    {
+      console.log("Data base failed")
+    }
+    else
+    {
+      console.log("Data base connected sucessfully...")
+      console.log(req.body)
+      clint.db("Newdata").collection("azadas").insertOne(req.body)
+    }
+  })
+  
+
+});
+
+
 
 module.exports = router;
